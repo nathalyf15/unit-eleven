01086408cc4b012660143718f1c082789b093c0c
Fiallos <25nfiallos@FVFY36M9J1WK.tld>
2022-03-31 12:41:54 -0500

    eleven

---
+++ app.js
@@ -0,0 +1,23 @@
+prompt(" hello, whats your name?");
+
+var human = 0;
+ 
+var computer = 0;
+
+var ran = Math.floor(Math.random() * 11)
+
+console.log(Math.floor(Math.random() * 11))
+
+console.log(ran)
+
+function askName() {
+    var name = prompt("Enter your name:")
+    var number = ran
+    console.log(Math.floor(Math.random() * 11)) + 1
+    alert(`Your new number is ${number}`)
+    prompt("would you like to keep going?");
+    
+
+} alert(askName()) //invoke
+
+
